53762f2023adb2097d9888e71c26056d79ae5321
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-05-30 20:22:40 +0600

    add

--- all-class/condition.js
+++ all-class/condition.js
@@ -1,7 +1,60 @@
 /*
  >,<,=,>=,<=,
+
+ !=,!==,!<,!> ,!<= , !>= ,== ,=== , ||or , &&and
+
+if(condition){
+}
+else{
+}
 */
 // console.log(5<7);
-console.log(5 >= 7);
+// console.log(5 >= 7);
 
-var orangePrice = 5;
+// var orangePrice = 5;
+
+// console.log(orangePrice);
+
+// var tomatoPrice = 10;
+// console.log(tomatoPrice);
+
+// var alurDam = 45;
+// console.log(alurDam);
+
+// console.log(10 == '10');
+
+// console.log(20 === '20');
+
+// var biriyaniPrice = 300;
+
+// if (biriyaniPrice < 200) {
+
+//     console.log('mama ak');
+
+// }
+// else {
+//     console.log('anno kisu');
+
+// }
+
+// var pizzaPrice = 400;
+
+// if (pizzaPrice <= 400) {
+
+//     console.log('mama ak pic ');
+// }
+// else {
+//     console.log('muri  kao');
+// }
+
+// var salary = 30000;
+// var isBcs = true;
+
+// if (salary >= 30000) {
+//     console.log('ABBA KOBUL');
+
+// }
+// else {
+//     console.log('NO');
+
+// }
