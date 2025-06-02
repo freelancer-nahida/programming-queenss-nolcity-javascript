e76c70f523f368d0f89056c224e44f015a9c0555
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-02 20:00:20 +0600

    end class

--- condition2.js
+++ condition2.js
@@ -16,3 +16,32 @@
 //     console.log('ber hoy');
 
 // };
+
+var budget = 300;
+
+if (budget >= 5000) {
+    //10% discount
+    var discount = budget * 10 / 100;
+    var payAmount = budget - discount;
+    console.log('you discount', discount, 'you pay', payAmount);
+}
+else if (budget >= 3000) {
+    // 5% discount
+
+    var discount = budget * 5 / 100;
+    var payAmount = budget - discount;
+    console.log('your discount', discount, 'you pay', payAmount);
+}
+
+else if (budget >= 1000) {
+    // 2% discount
+
+    var discount = budget * 2 / 100;
+    var payAmount = budget - discount;
+    console.log('your discount ', discount, 'you pay', payAmount);
+
+}
+else {
+    console.log('you have to pay hol amount', budget);
+
+};
