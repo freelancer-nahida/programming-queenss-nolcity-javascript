5b91cf5b8f03f79a93c43334e157e3fe8033d82b
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-04 20:13:39 +0600

    add1

---
+++ condition3.js
@@ -0,0 +1,67 @@
+// var mango = 20;
+// var berry = 40;
+// var def = berry - mango;
+
+// console.log(def);
+
+// var price = 2000;
+// var discount = price * 10 / 100;
+// var total = price - discount;
+// console.log('you have to pay', total);
+
+
+//jascript variable keyword 3 rokom 
+
+//var , let ,const
+
+// ver change kora jay
+
+// let angsik change kora jay 
+
+//const change kora jay
+
+
+// var num = 'sumu';
+// var num = 'ruma';
+
+// let num = 'sumu';
+// var num = 'ruma';
+
+// let num = 'sumu';
+// num = 'ruma';
+
+const num = 'sumu';
+// num = 'ruma';
+
+console.log(num);
+
+//akoy name akadik var declare kora jay
+
+
+/*
+
+naming convantion
+
+*/
+
+//1.No keyword in variable
+
+var true = 2;
+
+// No space and gap in variable name
+
+var mango price = 2;
+
+// 3. no quote
+
+var 'hello' = 9;
+
+// cannot start with number
+
+var 1laami = 2;
+
+ // 5. name case sentence 1.camel case 2.snack cask 3.pascal case
+
+ // 6. long variable _ use korte hobe 
+
+ var amar_nam = 'suma';
