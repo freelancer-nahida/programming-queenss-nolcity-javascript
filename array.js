686374f553241855904e023403b713d339888dff
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-08 06:02:21 +0600

    end arrey

---
+++ array.js
@@ -0,0 +1,35 @@
+const num1 = 12;
+const num2 = 13;
+const num3 = 15;
+const num = 19;
+
+const numbers = [12, 13, 15, 49];
+numbers[2] = 7;
+numbers.push(30);
+numbers.unshift(20);
+//for remove
+numbers.pop();
+
+numbers.shift();
+console.log(numbers);
+
+// const friends = ['abul', 'babul', 'habul', 'mokbul']
+
+// console.log(friends);
+
+//array likte 32 ta kaj korte hobe
+
+//use [ ] braket
+// use , for another value
+
+//set or update value by index
+
+
+
+// console.log(numbers[0]);
+
+let isLeader = true;
+if (!isLeader) {
+    console.log('hello');
+
+}
