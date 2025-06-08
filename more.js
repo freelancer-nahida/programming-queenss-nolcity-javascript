8f6a382ccacb6c4364d5b3512171b42937749ce0
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-08 07:31:10 +0600

    add more.js

---
+++ more.js
@@ -0,0 +1,42 @@
+// let girls = [];
+// girls.push('bellay')
+// girls.push('x')
+// girls.push('k')
+// girls.pop();
+// girls[1] = 'shuma';
+// console.log(girls);
+
+//puush and poa  seser nelement bad hoy;
+
+let khalToBhai = ['jodu', 'modu', 'modu', 'kodu'];
+
+
+console.log(khalToBhai.join('|'));
+//includes diya array data search kora hoy
+
+// if (khalToBhai.includes('sadu')) {
+//     console.log('ami khusi');
+
+// }
+// else {
+//     console.log('ami bissas kori na');
+
+// };
+// console.log(khalToBhai.indexOf('modu'));
+// console.log(khalToBhai.length);
+
+// let num = [4, 6, 7, 2, 4]
+// console.log(num.indexOf(8));
+// console.log(Array.isArray(khalToBhai));
+
+// let names = 'tut tut'
+
+// console.log(Array.isArray(names));
+
+let pizza = [3, 6, 8, 10, 22, 56];
+
+
+console.log(pizza.slice(1, 5));
+console.log(pizza);
+
+
