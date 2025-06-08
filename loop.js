27ea6e8400cd52b32ebbf73ad163bca24f597b6f
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-08 20:18:34 +0600

    add loop.js

---
+++ loop.js
@@ -0,0 +1,56 @@
+// let num = 0;
+
+// while (num < 5) {
+//     // console.log('Dute thako');
+//     console.log(num);
+//     num = num + 1;
+
+// }
+
+// let num = 0;
+
+// while (num < 10) {
+//     console.log(num);
+//     num = num + 1;
+// }
+
+//while loop declare korer way
+
+//1, declare a variable
+
+//2. while keyword are sathe condition add korte hobe 
+
+//3.{} braket are modde increment likte hobe
+
+// let num = 5;
+
+// while (num < 10) {
+//     console.log(num);
+//     //    num =num + 1;
+
+//     num++
+// }
+
+//num + 1 are shorthand num ++
+//num - 1 are shorthand num --
+
+// let num = 1;
+
+// while (num <= 100) {
+//     // console.log(num);
+//     num++
+//     if (num % 2 === 1) {
+//         console.log('odd num ', num);
+
+//     }
+
+// }
+let num = 1;
+
+while (num <= 100) {
+    if (num % 2 === 0) {
+        console.log('even number', num);
+
+    }
+    num++
+}
