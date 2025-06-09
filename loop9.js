7eae6474fdfdb34948e20d849b182c0a43b163d9
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-09 20:14:12 +0600

    add class 9

---
+++ loop9.js
@@ -0,0 +1,116 @@
+// let n = 10;
+// while (n >= 0) {
+//     n--;
+//     console.log(n);
+
+// }
+
+// for (let i = 0; i <= 20; i++) {
+
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+
+
+// }
+
+// for (let i = 0; i <= 30; i++) {
+//     if (i % 5 === 0) {
+//         console.log(i);
+
+//     }
+
+// }
+
+// let num = 0;
+// while (num <= 30) {
+//     if (num % 5 === 0) {
+//         num++
+//         console.log(num);
+
+//     }
+// }
+
+// for (let i = 0; i <= 30; i++) {
+//     if (i % 5 !== 0) {
+//         console.log(i);
+
+//     }
+
+// }
+
+// let i = 0;
+// while (i <= 20) {
+//     i++;
+//     if (i % 5 === 0) {
+//         console.log(i);
+
+//     }
+// }
+
+// let i = 0;
+// while (i <= 100) {
+//     i++
+//     if (i % 5 === 0 && i % 3 === 0) {
+//         console.log(i);
+
+//     }
+// }
+
+// let total = 0;
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 === 0) {
+//         total = total + i;
+//         // console.log(i, total);
+//     }
+// }
+// console.log('total', total);
+
+// for (let i = 0; i < 150; i++) {
+
+//     if (i >= 5) {
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// let i = 55;
+// while (i > 25) {
+//     // console.log(i);
+//     i--;
+//     // console.log(i);
+//     if (i < 50) {
+//         break;
+//     }
+//     console.log(i);
+// }
+//while a ak gor basi hoti pare
+// for a 
+
+// for (let i = 1; i < 10; i++) {
+//     if (i % 2 === 1) {
+//         continue;
+//     }
+
+//     console.log(i);
+
+// }
+// continue mane skib kora skib mane bad daya; 
+
+// 
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++
+// }
+// while (i < 5);
+
+// string declaration:
+
+const country = 'Bangladesh';
+const district = ' jhalokathi';
+const SD = `nalcity`;
+const thana = new String('Mollarhat')
+
+console.log(thana);
