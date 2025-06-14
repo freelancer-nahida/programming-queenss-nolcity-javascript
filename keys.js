d781db42c46188367f199993af8ccbfaae88c831
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-14 20:05:25 +0600

    add keysjs

---
+++ keys.js
@@ -0,0 +1,23 @@
+const favPlaces = {
+    ocean: 'kuakata',
+    jungle: 'sundarbon',
+    hills: 'sajek',
+    haritage: '60 gommbuj',
+
+    morePlaces: {
+        // sea: 'saint',
+        // hill: 'sitakundo',
+        // forest: 'amazon'
+
+        sweetPlace: {
+            place: 'mokka',
+            placeOne: 'amtoli',
+            placeTwo: 'kalapara'
+        }
+    }
+}
+// const keys = Object.keys(favPlaces);
+// const values = Object.values(favPlaces);
+// console.log(values);
+console.log(favPlaces.morePlaces.sweetPlace);
+
