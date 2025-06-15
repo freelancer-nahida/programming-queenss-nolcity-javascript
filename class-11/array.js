4d3797f64ae404e04c28a7f078a4f12b859851ea
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-15 10:37:56 +0600

    add class 11

---
+++ class-11/array.js
@@ -0,0 +1,78 @@
+// const num = [20, 33, 27, 26, 10];
+// num[2] = 28;
+// console.log(num);
+// const capital = 'Dacca';
+// // console.log(capital.length);
+// console.log(capital[1]);
+/////////////////////////////////
+// const subject = 'Chemistry';
+// const book = 'chemistry';
+// if (subject.toLowerCase() === book.toLowerCase()) {
+//     console.log('i am reading');
+
+// }
+// else {
+//     console.log('Fakibazi koro');
+
+// }
+
+//**********************
+
+// const nam = 'mayA';
+// const yourName = 'maYa';
+
+// if (yourName.toLowerCase() === nam.toLowerCase()) {
+//     console.log('hre power');
+
+// }
+// else {
+//     console.log('no');
+
+// }
+// const nam = 'ma  ya';
+// const yourName = 'maya';
+
+// if (yourName.trim() === nam.trim()) {
+//     console.log('hre power');
+
+// }
+// else {
+//     console.log('no');
+
+// }
+// const first = 'laboni';
+// const last = 'akter';
+// const fullname = first + '' + last;
+// // console.log(fullname);
+
+// const fullname2 = first.concat(' ').concat(last);
+// console.log(fullname2);
+
+
+// array te loop chslkanor jonno forof baboher 
+
+// const num = [20, 'maria', 27, 26, 10];
+
+// for (let nums of num) {
+//     console.log(nums);
+
+// }
+
+////////////
+
+// const names = ' bangladesh';
+// for (let name of names) {
+//     console.log(name);
+
+// }
+
+const names = '  sazid pore';
+let rev = '';
+
+for (let name of names) {
+    rev = name + rev
+    // console.log(rev);
+
+}
+
+console.log(rev);
