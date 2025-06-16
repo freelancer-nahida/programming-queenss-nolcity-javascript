84ce7c4840f702b8f7942248a00e5f886dcc4548
freelancer-nahida <freelancernahidasultana@gmail.com>
2025-06-16 13:24:04 +0600

    add class 13

---
+++ function.js
@@ -0,0 +1,56 @@
+// function fanOn() {
+
+//     console.log('age uthe dara');
+//     console.log(' then swith on koro');
+//     console.log('jaygy fere aso');
+
+// }
+// fanOn()
+
+// function roseDaw() {
+//     console.log('j amre pps korbe');
+//     console.log(' jake ami acept korbo');
+
+// }
+// roseDaw();
+
+// function square(number) {
+//     const borgo = number ** 3;
+//     console.log(borgo);
+
+// }
+// square(3);
+
+///////////////////////
+
+// function macherTorkari(mach, sobji, mosla) {
+
+//     console.log('ranna somponno');
+// }
+// macherTorkari('ilish', 'begun', 'tel');
+
+// function add(num1, num2) {
+//     const sum = num1 + num2;
+//     console.log(sum);
+
+// }
+// add(125, 225);
+// add(1200, 1300);
+
+// function add(a, b, c, d, e, f,) {
+//     const sum = a + b + c + d + e + f;
+//     console.log(sum);
+
+// }
+
+// add(1, 2, 3, 4, 5, 6);
+
+function tenTimes(number) {
+    const mult = number * 10;
+    // console.log(mult);
+    return mult
+
+}
+console.log(tenTimes(230));
+
+// return are call a result back kore
